c8502c718ac4647123201ba85bf97d9e352037e9
Alexandru Coarna <coarna.alexandru@gmail.com>
2020-09-15 07:37:05 +0300

    + changed the difference time value on late arrival to also take in consideration the time error

--- src/core/get-notify-message.ts
+++ src/core/get-notify-message.ts
@@ -47,7 +47,7 @@ const getNotifyMessage = async (notifyType: notifyType, needle: Needle) => {
         id: customConf.targetName || needle.id,
         customArea: needle.customArea ? needle.customArea.name || JSON.stringify(needle.customArea.position) : '',
         timetableCustomArea: needle.timetableCustomArea ? needle.timetableCustomArea.name || JSON.stringify(needle.timetableCustomArea.position) : '',
-        timeDifference: needle.time && needle.timetableCustomArea ? getTimeDifference(needle.time, needle.timetableCustomArea) : '',
+        timeDifference: needle.time && needle.timetableCustomArea ? getTimeDifference(needle.time, needle.timetableCustomArea, customConf) : '',
         sameLocationTime: customConf.sameLocationTime
     }
 
@@ -59,8 +59,6 @@ const getNotifyMessage = async (notifyType: notifyType, needle: Needle) => {
     let message = languageMessages[notifyType];
 
     Object.keys(map).forEach(key => message = message.replace(new RegExp(`{{${ key }}}`, 'g'), map[key]));
-
-    console.log(message);
 
     return message;
 }
